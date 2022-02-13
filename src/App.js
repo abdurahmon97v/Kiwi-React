836c0c33e983c27906c4a9f61f74c1d3a99cd082
abdumovi <abdumovi@gmail.com>
2022-02-13 13:49:06 +0500

    end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { AppContext } from "./contexts/AppContext";
+import { Home, Info, Layaut, SignIn, BigSearch } from "./pages";
+import { Route, Routes } from "react-router-dom";
+import { ru } from "./lang/lang";
+import { GetItems, GetCategory } from "./api/api";
+import { Formin , Register} from "./components";
 
 function App() {
+  const [nameval,setNameVal] = React.useState('');
+  const [language, setLanguage] = React.useState(ru);
+  const [category, setCategory] = React.useState('1');
+  const [allItem, setAllItem] = React.useState([]);
+  const [search, setSearch] = React.useState('');
+  
+  useEffect(async () => {
+    if (category === '1') {
+      const result = await GetItems();
+      setAllItem([...result]);
+    }
+    else {
+      const result = await GetCategory(category);
+      setAllItem([...result]);
+    }
+  }, [category]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider
+      value={{
+        nameval,setNameVal,
+        language,
+        setLanguage, category,
+        setCategory, setAllItem,
+        allItem, search, setSearch
+      }}>
+      <Routes>
+        <Route path="*" element={<Layaut />}>
+          <Route index element={<Home />} />
+          <Route path="info/:id" element={<Info />} />
+          <Route path="signin/*" element={<SignIn />}>
+              <Route path='signup' element={<Formin />} />
+              <Route path='signup/register' element={<Register />} />
+          </Route>
+          <Route path="search" element={<BigSearch />} />
+        </Route>
+      </Routes>
+    </AppContext.Provider>
   );
 }
 
